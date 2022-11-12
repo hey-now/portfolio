0c457a459b46967b72ee24a2c54f71871ff531a1
hey-now <rubinand22@gmail.com>
2022-11-12 07:43:28 -0800

    addinng nav and views

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,7 +1,15 @@
 module.exports = {
-    about
+    about,
+    projects,
+    contact
 }
 
 function about(req, res) {
     res.render('portfolio/about', { title: 'Meet the Developer' });
+}
+function projects(req, res) {
+    res.render('portfolio/projects', { title: 'My Work' });
+}
+function contact(req, res) {
+    res.render('portfolio/contact', { title: 'Contact Me!' });
 }
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -4,5 +4,7 @@ var portfolioCtrl = require('../controllers/portfolio');
 
 // all posts start with /portfolio
 router.get('/about', portfolioCtrl.about);
+router.get('/projects', portfolioCtrl.projects);
+router.get('/contact', portfolioCtrl.contact);
 
 module.exports = router;
